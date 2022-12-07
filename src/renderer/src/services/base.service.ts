3e7af52e99bcf6331b830b3823ce5a64ae28ed7c
PauloUbirajara <ubirajara3@hotmail.com>
2022-12-07 16:30:07 -0300

    update: remove base.service.ts

--- src/renderer/src/services/base.service.ts
+++
@@ -1,9 +0,0 @@
-import { Citation } from '@renderer/types/citation.type'
-import { Details } from '@renderer/types/details.type'
-
-export interface BaseService {
-  abortPendingRequests(): void
-  fetchUserCitations(): Promise<Citation[]>
-  fetchUserDetails(): Promise<Details>
-  visitAuthor(authorUrl: string): Promise<void>
-}
