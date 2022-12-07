46fcd4d8341a4b9c7d887ae66771175e25aefe46
PauloUbirajara <ubirajara3@hotmail.com>
2022-12-07 16:28:14 -0300

    feature: add breadcrumb for navigation
    update: add 'Results' path

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,20 +1,26 @@
 import { Box } from '@chakra-ui/react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { Header } from './components/Header'
-import { Main } from './pages/Main'
-import { Preview } from './pages/Preview'
+import { Header } from '@renderer/components/Header'
+import { CustomBreadcrumb } from '@renderer/components/CustomBreadcrumb'
+import { Main } from '@renderer/pages/Main'
+import { Preview } from '@renderer/pages/Preview'
+import { Results } from '@renderer/pages/Results'
 
 const App = (): JSX.Element => {
   return (
-    <Box h={'100vh'} pt={'16'}>
+    <Box pt={'16'}>
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/preview" element={<Preview />} />
-          <Route path="*" element={<h1>404</h1>} />
-        </Routes>
+        <Box p={5}>
+          <CustomBreadcrumb />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/preview" element={<Preview />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="*" element={<h1>404</h1>} />
+          </Routes>
+        </Box>
       </BrowserRouter>
     </Box>
   )
