89faf153f4dc7d364e0f4fa23721dbfcff549170
PauloUbirajara <ubirajara3@hotmail.com>
2022-12-11 21:56:58 -0300

    update: update fetch and save interfaces with YearType

--- src/renderer/src/services/baseFetch.service.ts
+++ src/renderer/src/services/baseFetch.service.ts
@@ -1,9 +1,11 @@
 import { Citation } from '@renderer/types/citation.type'
 import { Details } from '@renderer/types/details.type'
+import { YearType } from '@renderer/types/years.type'
 
 export interface BaseFetchService {
   abortPendingRequests(): void
   fetchUserCitations(): Promise<Citation[]>
   fetchUserDetails(): Promise<Details>
+  setupYearsToFetch(years: YearType): Promise<void>
   visitAuthor(authorUrl: string): Promise<void>
 }
--- src/renderer/src/services/baseSave.service.ts
+++ src/renderer/src/services/baseSave.service.ts
@@ -1,8 +1,11 @@
 import { Spreadsheet } from '@renderer/types/spreadsheet.type'
+import { YearType } from '@renderer/types/years.type'
 
 export interface BaseSaveService {
   load(data: Spreadsheet): void
   getData(): Spreadsheet
+  getYears(): YearType
   save(): void
+  setupYears(years: YearType): void
   // TODO Exportar para PDF (?) - pode ser um outro tipo de servico
 }
