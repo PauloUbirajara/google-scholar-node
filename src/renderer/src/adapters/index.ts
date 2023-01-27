169cdb9353415fc8db9308ec2af3a9a1cc9c8320
PauloUbirajara <ubirajara3@hotmail.com>
2023-01-27 00:48:55 -0300

    refactor: remove unused adapters

--- src/renderer/src/adapters/index.ts
+++
@@ -1,5 +0,0 @@
-import { BaseSpreadsheetAdapter } from './base.adapter'
-import ExcelJsAdapter from './exceljs.adapter'
-import SheetJsAdapter from './sheetjs.adapter'
-
-export const adapters: BaseSpreadsheetAdapter[] = [ExcelJsAdapter, SheetJsAdapter]
